f58d9b7ce97405e8f4aef2c9a245d62291224816
mingiyeom <mingiyeom@kaist.ac.kr>
2019-10-04 08:28:06 +0900

    export

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+
+#include <GeneralUtility.h>
 #include <Photographer.h>
 
 #include <GeneralMesh.h>
@@ -7,193 +10,177 @@
 
 #include <BoundingBox.h>
 #include <PictureComparison.h>
-#include <GeneralUtility.h>
-
-#include <iostream>
-
 #include <OBJController.h>
 #include <Picking.h>
 
-#include <fstream>
-#include <string>
+std::string obj_path, obj_list_name, tex_path, tex_list_name, root_path;
+std::vector <std::string> obj_list, tex_list;
+int n_segment = 36;
+bool is_front;
 
-#include <igl/unproject_ray.h>
-#include <igl/unproject_onto_mesh.h>
+void preprocessing_front() {
+    std::cout << "\n\n//---------------------------------\n Preprocessing Front, start!\n//---------------------------------\n\n";
+    Photographer myPhotographer;
+    myPhotographer.addCameraRingRoutine(n_segment, 0.0, 2.0f);
 
-#include <direct.h>
+    for (auto i = 0; i < obj_list.size(); ++i) {
+        std::cout << ">>> The Mesh '" + obj_list[i] + "' is loaded!" << std::endl;
+
+        std::string image_path = root_path + mg::ssplit(obj_list[i], '.')[0] + "/";
+        std::string obj_full_path = obj_path + obj_list[i];
+        std::string tex_full_path = tex_path + tex_list[i];
+
+        std::string obj_name = mg::ssplit(obj_list[i], '.')[0];
+        std::string prefix = obj_name + "_";
+
+
+        //--------------------------------------------------------------------------------------------------------------------------
+        GeneralMeshTexture myGeneralMeshTexture(obj_full_path.c_str(), tex_full_path.c_str());
+
+        myPhotographer.setInit(&myGeneralMeshTexture);
+        myPhotographer.setShader(Shader::ShaderTypes::FULL_SHADER);
+        myPhotographer.saveImageCamerasParamsCV(image_path, prefix);
+        std::vector<std::string> texmesh_name_list = myPhotographer.renderToImages(image_path, "texmesh_view_" + prefix);
+        mg::writeTxt(texmesh_name_list, image_path + "texmesh_name_list.txt");
+        //myPhotographer.viewScene(true);
+        //--------------------------------------------------------------------------------------------------------------------------
+        GeneralMeshID myGeneralMeshID(obj_full_path.c_str());
+        myPhotographer.setObject(&myGeneralMeshID);
+
+        myPhotographer.setShader(Shader::ShaderTypes::FACEID_SHADER);
+        myPhotographer.saveImageCamerasParamsCV(image_path, prefix);
+        std::vector<std::string> id_name_list = myPhotographer.renderToImages(image_path, "id_view_" + prefix);
+        mg::writeTxt(id_name_list, image_path + "id_name_list.txt");
+        //myPhotographer.viewScene(true);
+
+        ////--------------------------------------------------------------------------------------------------------------------------
+        texmesh_name_list = mg::readTxt(image_path + "texmesh_name_list.txt");
+        id_name_list = mg::readTxt(image_path + "id_name_list.txt");
+
+        std::vector<std::string> bound_texmesh_name_list, bound_id_name_list, test_id_list; // test_id is the bound_texmesh_name without extension
+        for (auto k = 0; k < texmesh_name_list.size(); ++k) {
+            std::string load_path = image_path + texmesh_name_list[k];
+            std::string bound_texmesh_name = mg::convertExtension("bd_" + texmesh_name_list[k], "jpg"); // <- need to convert jpg
+            std::string test_id_name = mg::convertExtension("bd_" + texmesh_name_list[k], ""); // <- need to convert (blank)
+            std::string save_path = image_path + bound_texmesh_name; // <- need to convert jpg
+            BoundingBox myBoundingbox(load_path);
+            myBoundingbox.saveTexBounded(save_path);
+            bound_texmesh_name_list.push_back(bound_texmesh_name);
+            test_id_list.push_back(test_id_name);
+
+            load_path = image_path + id_name_list[k];
+            std::string bound_id_name = "bd_" + id_name_list[k]; // <- need to convert jpg
+            save_path = image_path + bound_id_name; // <- need to convert jpg
+            BoundingBox myBoundingbox2(load_path);
+            myBoundingbox2.saveTexBounded(save_path);
+            bound_id_name_list.push_back(bound_id_name);
+        }
+
+        mg::writeTxt(bound_texmesh_name_list, image_path + "bd_texmesh_name_list.txt");
+        mg::writeTxt(bound_texmesh_name_list, image_path + "/ce2p/" + "bd_texmesh_name_list.txt");
+        mg::writeTxt(test_id_list, image_path + "/ce2p/" + "test_id.txt");
+        mg::writeTxt(bound_id_name_list, image_path + "bd_id_name_list.txt");
+    }
+
+
+    std::cout << "\n\n//---------------------------------\n Preprocessing Front, done!\n//---------------------------------\n\n";
+}
+
+void preprocessing_back() {
+    std::cout << "\n\n//---------------------------------\n Preprocessing Back, start!\n//---------------------------------\n\n";
+    std::vector<std::string> ce2p_texmesh_name_list, bound_id_name_list;
+
+    for (auto i = 0; i < obj_list.size(); ++i) {
+        std::cout << ">>> The Mesh '" + obj_list[i] + "' is loaded!" << std::endl;
+
+        std::string image_path = root_path + mg::ssplit(obj_list[i], '.')[0] + "/";
+        std::string parsing_path = image_path + "ce2p/";
+        ce2p_texmesh_name_list = mg::readTxt(parsing_path + "bd_texmesh_name_list.txt"); // <- bound + ce2p
+        bound_id_name_list = mg::readTxt(image_path + "bd_id_name_list.txt"); // <- bound
+
+        std::string obj_full_path = obj_path + obj_list[i];
+        GeneralMesh object(obj_full_path.c_str());
+
+        vector<string> face_color;
+        map<string, array<float, 3>> color_table;
+        vector<map<string, int>> face_color_counter_;
+
+        PictureComparison myPictureComparison(object.getFaces().rows());
+        myPictureComparison.setColorTable(tex_path + "coloring_table_ce2p.txt");
+        myPictureComparison.initFaceColorTable();
+
+        for (int k = 0; k < 36; ++k) {
+            std::string ce2p_texmesh_full_path = mg::convertExtension(parsing_path + ce2p_texmesh_name_list[k], "png");
+            std::string bound_id_full_path = image_path + bound_id_name_list[k];
+
+            myPictureComparison.setTex(ce2p_texmesh_full_path, bound_id_full_path);
+            myPictureComparison.computeIdColor();
+
+
+            face_color = myPictureComparison.getFaceColor();
+            color_table = myPictureComparison.getColorTableFloat();
+
+            std::cout << "  Merge (" << k+1 << "/" << n_segment << ") Picture" << std::endl;
+
+        }
+        myPictureComparison.setColorTable(tex_path + "coloring_table_visualization.txt");
+        color_table = myPictureComparison.getColorTableFloat();
+
+        ParsingMesh myParsingObj(obj_full_path.c_str(), face_color, color_table);
+        myParsingObj.saveOBJ(tex_path + "uv_coloring.txt", obj_path + "Parsing" + obj_list[i]);
+
+        std::cout << "\n\n//---------------------------------\n Preprocessing Back, done!\n//---------------------------------\n\n";
+    }
+}
+
+void test() {
+    std::cout << "\n\n//---------------------------------\n Test, start!\n//---------------------------------\n\n";
+    for (auto i = 0; i < obj_list.size(); ++i) {
+        std::cout << ">>> The Mesh '" + obj_list[i] + "' is loaded!" << std::endl;
+
+        std::string obj_full_path_test = obj_path + "Parsing" + obj_list[i];
+        std::string tex_full_path_test = tex_path + "coloring_visualization.jpg";
+
+        GeneralMeshTexture myGeneralMeshTextureTest(obj_full_path_test.c_str(), tex_full_path_test.c_str());
+
+        Photographer myPhotographer;
+        myPhotographer.setInit(&myGeneralMeshTextureTest);
+        myPhotographer.setShader(Shader::ShaderTypes::FULL_SHADER);
+        myPhotographer.viewScene(true);
+    }
+
+    std::cout << "\n\n//---------------------------------\n Test, done!\n//---------------------------------\n\n";
+}
 
 int main(int argc, char *argv[])
 {
-    std::string obj_path, obj_list_name, tex_path, tex_list_name, root_path;
-    std::string in_line;
-    std::vector <std::string> obj_list, tex_list;
-    ifstream objfile, texfile;
 
-    ////
+    ////initialize path and flag
     obj_path = argv[1]; obj_path += "/";
     obj_list_name = argv[2];
     tex_path = argv[3]; tex_path += "/";
     tex_list_name = argv[4];
     root_path = argv[5]; root_path += "/";
-    bool isFront = std::stoi(std::string(argv[6]));
-    ////
-
+    bool is_front = std::stoi(std::string(argv[6]));
+    
     obj_list = mg::readTxt(obj_path + obj_list_name);
     tex_list = mg::readTxt(tex_path + tex_list_name);
+    ////
 
-    Photographer photographer;
-    if (isFront) {
-        photographer.addCameraRingRoutine(36, 0.0, 2.0f);
+    if (is_front) {
+        preprocessing_front();
+    }
+    
+    ////--------------------------------------------------------------------------------------
+    ////preprocessing_middle()
+    ////CE2p
+    ////
+    ////--------------------------------------------------------------------------------------
+
+    if (!is_front) {
+        preprocessing_back();
+        test();
     }
 
-    ////for (auto i = 0; i < obj_list.size() && isFront; ++i)
-    //for (auto i = 4; i < 5 && isFront; ++i) {
-    //    std::string image_path = root_path + mg::ssplit(obj_list[i],'.')[0] + "/";
-    //    std::string obj_full_path = obj_path + obj_list[i];
-    //    std::string tex_full_path = tex_path + tex_list[i];
-
-    //    std::string obj_name = mg::ssplit(obj_list[i], '.')[0];
-    //    std::string prefix = obj_name + "_";
-
-    //    std::cout << "The Mesh " + obj_name + " is Loaded!" << std::endl;
-
-    //    //--------------------------------------------------------------------------------------------------------------------------
-    //    GeneralMeshTexture myGeneralMeshTexture(obj_full_path.c_str(), tex_full_path.c_str());
-
-    //    photographer.setInit(&myGeneralMeshTexture);
-    //    photographer.setShader(Shader::ShaderTypes::FULL_SHADER);
-    //    photographer.saveImageCamerasParamsCV(image_path, prefix);
-    //    std::vector<std::string> texmesh_name_list = photographer.renderToImages(image_path, "texmesh_view_" + prefix);
-    //    mg::writeTxt(texmesh_name_list, image_path + "texmesh_name_list.txt");
-    //    //photographer.viewScene(true);
-    //    //--------------------------------------------------------------------------------------------------------------------------
-    //    GeneralMeshID myGeneralMeshID(obj_full_path.c_str());
-    //    photographer.setObject(&myGeneralMeshID);
-
-    //    photographer.setShader(Shader::ShaderTypes::FACEID_SHADER);
-    //    photographer.saveImageCamerasParamsCV(image_path, prefix);
-    //    std::vector<std::string> id_name_list = photographer.renderToImages(image_path, "id_view_" + prefix);
-    //    mg::writeTxt(id_name_list, image_path + "id_name_list.txt");
-    //    //photographer.viewScene(true);
-
-    //    ////--------------------------------------------------------------------------------------------------------------------------
-    //    texmesh_name_list = mg::readTxt(image_path + "texmesh_name_list.txt");
-    //    id_name_list = mg::readTxt(image_path + "id_name_list.txt");
-
-    //    std::vector<std::string> bound_texmesh_name_list, bound_id_name_list, test_id_list; // test_id is the bound_texmesh_name without extension
-    //    for (auto k = 0; k < texmesh_name_list.size(); ++k) {
-    //        std::string load_path = image_path + texmesh_name_list[k];
-    //        std::string bound_texmesh_name = mg::convertExtension("bd_" + texmesh_name_list[k], "jpg"); // <- need to convert jpg
-    //        std::string test_id_name = mg::convertExtension("bd_" + texmesh_name_list[k], ""); // <- need to convert (blank)
-    //        std::string save_path =  image_path + bound_texmesh_name; // <- need to convert jpg
-    //        BoundingBox myBoundingbox(load_path);
-    //        myBoundingbox.saveTexBounded(save_path);
-    //        bound_texmesh_name_list.push_back(bound_texmesh_name);
-    //        test_id_list.push_back(test_id_name);
-
-    //        load_path = image_path + id_name_list[k];
-    //        std::string bound_id_name = "bd_" + id_name_list[k]; // <- need to convert jpg
-    //        save_path = image_path + bound_id_name; // <- need to convert jpg
-    //        BoundingBox myBoundingbox2(load_path);
-    //        myBoundingbox2.saveTexBounded(save_path);
-    //        bound_id_name_list.push_back(bound_id_name);
-    //    }
-
-    //    mg::writeTxt(bound_texmesh_name_list, image_path + "bd_texmesh_name_list.txt");
-    //    mg::writeTxt(bound_texmesh_name_list, image_path + "/ce2p/" + "bd_texmesh_name_list.txt");
-    //    mg::writeTxt(test_id_list, image_path + "/ce2p/" + "test_id.txt");
-    //    mg::writeTxt(bound_id_name_list, image_path + "bd_id_name_list.txt");
-    //}
-
-    //////--------------------------------------------------------------------------------------
-    //////
-    //////CE2p
-    //////
-    //////--------------------------------------------------------------------------------------
-
-    //std::vector<std::string> ce2p_texmesh_name_list, bound_id_name_list;
-
-    //for (auto i = 4; i < 5 && !isFront; ++i) {
-    //    std::string image_path = root_path + mg::ssplit(obj_list[i], '.')[0] + "/";
-    //    std::string parsing_path = image_path + "ce2p/";
-    //    ce2p_texmesh_name_list = mg::readTxt(parsing_path + "bd_texmesh_name_list.txt"); // <- bound + ce2p
-    //    bound_id_name_list = mg::readTxt(image_path + "bd_id_name_list.txt"); // <- bound
-
-    //    std::string obj_full_path = obj_path + obj_list[i];
-    //    GeneralMesh object(obj_full_path.c_str());
-
-    //    vector<string> face_color;
-    //    map<string, array<float, 3>> color_table;
-    //    vector<map<string, int>> face_color_counter_;
-
-    //    PictureComparison myPictureComparison(object.getFaces().rows());
-    //    //std::cout << "object.getFaces().rows(): " << object.getFaces().rows() << std::endl;
-    //    myPictureComparison.setColorTable("D:/Data/GigaKorea/scans_obj/mman_coloring_fit_fit2.txt");
-    //    myPictureComparison.initFaceColorTable();
-
-    //    for (int k = 0; k < 36; ++k) {
-    //        std::string ce2p_texmesh_full_path = mg::convertExtension(parsing_path + ce2p_texmesh_name_list[k], "png");
-    //        std::string bound_id_full_path = image_path + bound_id_name_list[k];
-
-    //        myPictureComparison.setTex(ce2p_texmesh_full_path, bound_id_full_path);
-    //        myPictureComparison.computeIdColor();
-
-    //        
-    //        face_color = myPictureComparison.getFaceColor();
-    //        color_table = myPictureComparison.getColorTableFloat();
-
-    //        std::cout << "k: " << k << std::endl;
-
-    //    }
-    //    myPictureComparison.setColorTable("D:/Data/GigaKorea/scans_obj/mman_coloring_fit_fit.txt");
-    //    color_table = myPictureComparison.getColorTableFloat();
-
-    //    ParsingMesh myParsingObj(obj_full_path.c_str(), face_color, color_table);
-    //    Photographer photographerParsing(&myParsingObj);
-    //    photographerParsing.setShader(Shader::ShaderTypes::MOSAIC_SHADER, Shader::ShaderTypes::MOSAIC_SHADER);
-
-    //    //photographer.addCameraToPosition(0.0f, 0.0f, 1.0f, 4.0f);
-    //    //    //photographer.addCameraToPosition(1.0f, -0.5f, 2.0f, 4.0f);
-    //    //    //photographer.addCameraToPosition(-1.0f, 0.0f, 1.0f, 4.0f);
-    //    //    //photographer.addCameraToPosition(-0.0f, -0.0f, -1.0f, 4.0f);
-    //    //    //photographer.addCameraToPosition(-1.0f, 0.5f, -2.0f, 4.0f);
-    //    //    //photographer.addCameraToPosition(1.0f, -0.0f, -1.0f, 4.0f);
-    //    //photographerParsing.renderToImages(argv[5]);
-    //    //photographerParsing.saveImageCamerasParamsCV(argv[5]);
-    //    myParsingObj.saveOBJ(tex_path + "uv_coloring.txt", obj_path + "Parsing" + obj_list[i]);
-    //    photographerParsing.viewScene(true);
-
-    //}
-    //
-    //std::string obj_full_path_test = obj_path + "Parsing" + obj_list[4];
-    //std::string tex_full_path_test = tex_path + "color_map.jpg";
-
-    //GeneralMeshTexture myGeneralMeshTextureTest(obj_full_path_test.c_str(), tex_full_path_test.c_str());
-
-    //photographer.setInit(&myGeneralMeshTextureTest);
-    //photographer.setShader(Shader::ShaderTypes::FULL_SHADER);
-    //photographer.viewScene(true);
-
-    //    myPictureComparison.setColorTable("D:/Data/GigaKorea/scans_obj/mman_coloring_fit_fit.txt");
-    //    face_color = myPictureComparison.getFaceColor();
-    //    color_table = myPictureComparison.getColorTableFloat();
-
-    //    ParsingMesh myParsingObj(argv[1], face_color, color_table);
-    //    Photographer photographerParsing(&myParsingObj);
-    //    photographerParsing.setShader(Shader::ShaderTypes::MOSAIC_SHADER, Shader::ShaderTypes::MOSAIC_SHADER);
-
-    //--------------------------------------------------------------------------------------
-    //    //Photographer photographerVisual(&object);
-    //    //photographer.addCameraToPosition(0.0f, 0.0f, 1.0f, 4.0f);
-    //    //photographer.addCameraToPosition(1.0f, -0.5f, 2.0f, 4.0f);
-    //    //photographer.addCameraToPosition(-1.0f, 0.0f, 1.0f, 4.0f);
-    //    //photographer.addCameraToPosition(-0.0f, -0.0f, -1.0f, 4.0f);
-    //    //photographer.addCameraToPosition(-1.0f, 0.5f, -2.0f, 4.0f);
-    //    //photographer.addCameraToPosition(1.0f, -0.0f, -1.0f, 4.0f);
-
-    //    //photographerVisual.setShader(Shader::ShaderTypes::FULL_SHADER, Shader::ShaderTypes::FULL_SHADER);
-    //    //photographerVisual.renderToImages(argv[5]);
-    //    //photographerVisual.saveImageCamerasParamsCV(argv[5]);
-    //    //photographerParsing.viewScene(true);
-    //
+    return 0;
 }
